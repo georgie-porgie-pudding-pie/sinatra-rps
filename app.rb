8d67eec5ac3cc4dbe2af5c6c73b751287ba639d9
georgie-porgie-pudding-pie <gkobalya@chicagobooth.edu>
2025-04-07 23:21:49 +0000

    FINAL

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "rake"
+
+options = ["rock", "paper", "scissors"]
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  @play = options.sample
+  erb(:rock)
+end
+
+get("/paper") do
+  @play = options.sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @play = options.sample
+  erb(:scissors)
 end
